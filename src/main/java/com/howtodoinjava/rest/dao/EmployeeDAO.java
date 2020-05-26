9a3f5ffe1538c705109b8792fdcb318e48262961
SAPANDI <JAnuary@143>
2020-05-26 16:19:14 +0530

    updated latest code

--- src/main/java/com/howtodoinjava/rest/dao/EmployeeDAO.java
+++ src/main/java/com/howtodoinjava/rest/dao/EmployeeDAO.java
@@ -1,27 +1,23 @@
 package com.howtodoinjava.rest.dao;
 
-import org.springframework.stereotype.Repository;
-
 import com.howtodoinjava.rest.model.Employee;
 import com.howtodoinjava.rest.model.Employees;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public class EmployeeDAO 
-{
+public class EmployeeDAO {
     private static Employees list = new Employees();
-    
-    static 
-    {
-        list.getEmployeeList().add(new Employee(1, "Lokesh", "Gupta", "howtodoinjava@gmail.com"));
-        list.getEmployeeList().add(new Employee(2, "Alex", "Kolenchiskey", "abc@gmail.com"));
-        list.getEmployeeList().add(new Employee(3, "David", "Kameron", "titanic@gmail.com"));
+
+    static {
+        list.getEmployeeList().add(new Employee(1, "Rajesh", "Singh", "testemail@gmail.com"));
+        list.getEmployeeList().add(new Employee(2, "Ajay", "Kumar", "ajay564@gmail.com"));
+        list.getEmployeeList().add(new Employee(3, "Sagar", "Pandita", "sam123@gmail.com"));
     }
-    
-    public Employees getAllEmployees() 
-    {
+
+    public Employees getAllEmployees() {
         return list;
     }
-    
+
     public void addEmployee(Employee employee) {
         list.getEmployeeList().add(employee);
     }
